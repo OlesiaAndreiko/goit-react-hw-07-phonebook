cbeeafbcb7a015592eeb8bb42178b40f392b1ece
Olesia Andreiko <olesikmail@gmail.com>
2023-02-24 09:53:29 +0200

    comment all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,30 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { ContactList } from '../components/ContactList/ContactList';
+// import { ContactList } from '../components/ContactList/ContactList';
 // import { Filter } from './Filter/Filter';
 // import { FormContact } from './FormContact/FormContact';
 import { Container } from './Container/Container.styled';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchContacts } from 'redux/operations';
-import { getError, getIsLoading } from 'redux/selectors';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import { fetchContacts } from 'redux/operations';
+// import { getError, getIsLoading } from 'redux/selectors';
 
 export const App = () => {
-  const dispatch =useDispatch();
-  const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);
+  // const dispatch =useDispatch();
+  // const isLoading = useSelector(getIsLoading);
+  // const error = useSelector(getError);
 
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
    
   return (
     <Container>
       {/* <FormContact /> */}
       {/* <Filter/> */}
-      {isLoading && !error && <b>Request in progress...</b>}
-      <ContactList />
+      {/* {isLoading && !error && <b>Request in progress...</b>} */}
+      <h1>Hello world</h1>
+      {/* <ContactList /> */}
       <ToastContainer position="top-center" theme="colored" />
     </Container>
   );
