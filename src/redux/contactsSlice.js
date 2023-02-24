77611cf5fa311068bfca32fb87ce89ce37aa6f44
Olesia Andreiko <olesikmail@gmail.com>
2023-02-24 12:10:48 +0200

    Delete superfluous comments.

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -10,45 +10,18 @@ const contactsSlice = createSlice({
   },
   extraReducers: builder =>
     builder
-      // .addCase(fetchContacts.pending, (state) => {
-      //   state.isLoading = true;
-      // })
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        // state.isLoading = false;
-        // state.error = null;
         state.items = action.payload;
       })
-      // .addCase(fetchContacts.rejected, (state, action) => {
-      //   state.isLoading = false;
-      //   state.error = action.payload;
-      // })
-      // .addCase(addContact.pending, (state) => {
-      //   state.isLoading = true;
-      // })
       .addCase(addContact.fulfilled, (state, action) => {
-        // state.isLoading = false;
-        // state.error = null;
         state.items.push(action.payload);
       })
-      // .addCase(addContact.rejected, (state, action) => {
-      //   state.isLoading = false;
-      //   state.error = action.payload;
-      // })
-      // .addCase(deleteContact.pending, (state) => {
-      //   state.isLoading = true
-      // })
       .addCase(deleteContact.fulfilled, (state, action) => {
-        // state.isLoading = false;
-        // state.error = null;
         const index = state.items.findIndex(
           contact => contact.id === action.payload
         );
         state.items.splice(index, 1);
       })
-      // .addCase(deleteContact.rejected, (state, action) => {
-      //   state.isLoading = false;
-      //   state.error = action.payload;
-      // })
       .addMatcher(
         isAnyOf(
           fetchContacts.pending,
