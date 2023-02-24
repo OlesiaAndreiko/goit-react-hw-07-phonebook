2e90f8f89fe98e04a2230b7dd30f1dcfa7dec270
Olesia Andreiko <olesikmail@gmail.com>
2023-02-24 10:00:33 +0200

    Added fetchAll

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,25 +4,25 @@ import 'react-toastify/dist/ReactToastify.css';
 // import { Filter } from './Filter/Filter';
 // import { FormContact } from './FormContact/FormContact';
 import { Container } from './Container/Container.styled';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useEffect } from 'react';
-// import { fetchContacts } from 'redux/operations';
-// import { getError, getIsLoading } from 'redux/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+import { getError, getIsLoading } from 'redux/selectors';
 
 export const App = () => {
-  // const dispatch =useDispatch();
-  // const isLoading = useSelector(getIsLoading);
-  // const error = useSelector(getError);
+  const dispatch =useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
 
-  // useEffect(() => {
-  //   dispatch(fetchContacts());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
    
   return (
     <Container>
       {/* <FormContact /> */}
       {/* <Filter/> */}
-      {/* {isLoading && !error && <b>Request in progress...</b>} */}
+      {isLoading && !error && <b>Request in progress...</b>}
       <h1>Hello world</h1>
       {/* <ContactList /> */}
       <ToastContainer position="top-center" theme="colored" />
