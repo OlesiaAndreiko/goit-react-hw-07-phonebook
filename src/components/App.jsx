44ac88fbc10967e027d5b4bbf1a72d5ecac1c656
Olesia Andreiko <olesikmail@gmail.com>
2023-02-26 15:42:30 +0200

    Fix App and ContactList.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,25 +4,13 @@ import { ContactList } from '../components/ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { FormContact } from './FormContact/FormContact';
 import { Container } from './Container/Container.styled';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchContacts } from 'redux/operations';
-import { selectError, selectIsLoading } from 'redux/selectors';
 
 export const App = () => {
-  const dispatch =useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
    
   return (
     <Container>
       <FormContact />
       <Filter/>
-      {isLoading && !error && <b>Request in progress...</b>}
       <ContactList />
       <ToastContainer position="top-center" theme="colored" />
     </Container>
